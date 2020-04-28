f0384134c06a6b1a93d0a8c86a0fa51bc1573aed
Roberto Kai Hsi Hoo <prof.robertohoo@gmail.com>
2020-04-28 15:29:35 -0300

    ok

--- WinFormApp1/Program.fs
+++ WinFormApp1/Program.fs
@@ -1,7 +1,20 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System.Drawing
+open System.Windows.Forms
+open System
+open System.IO
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+// --------------------------------------------------------------------------
+// Modelo de Windows Form com um botão
+// --------------------------------------------------------------------------
+// Create main form window
+let mainForm = new Form(Width = 620, Height = 450, Text = "First F# Windows Form")
+let mutable button = 
+    new Button(Location=new Point(40,20), Width = 160, Height = 40, Text = "Click me to close!")
+
+button.Click.Add(fun _ -> Application.Exit() |> ignore)
+mainForm.Controls.Add(button)
+// Entry-point for the graphical version   
+[<STAThread>]
+do // Specify event handlers - a function called when button is clicked
+   Application.EnableVisualStyles()
+   Application.Run(mainForm)
